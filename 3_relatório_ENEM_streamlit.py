d187834441956af7e444018e19f1f146bd169f1e
ericbitos <erick.jost@gmail.com>
2026-04-06 21:35:45 -0300

    atualizando os gráficos e a base de dados

--- 3_relatório_ENEM_streamlit.py
+++ 3_relatório_ENEM_streamlit.py
@@ -11,44 +11,42 @@ engine = create_engine(db_url)
 
 @st.cache_data
 def load_data():    
-    freq_lingua = pd.read_sql('SELECT tp_lingua, COUNT(*) AS frequencia FROM public.ed_enem_2024_resultados GROUP BY tp_lingua ORDER BY frequencia DESC', engine)
+    freq_lingua = pd.read_sql('SELECT tp_lingua, COUNT(*) AS frequencia FROM public.ed_enem_2024_resultados_amos_per GROUP BY tp_lingua ORDER BY frequencia DESC', engine)
     return freq_lingua
 
 @st.cache_data
 def load_uf():
-    df_top5_uf = pd.read_sql('SELECT nome_uf_prova, COUNT(*) AS frequencia FROM public.ed_enem_2024_resultados GROUP BY nome_uf_prova ORDER BY frequencia DESC LIMIT 5', engine)
-    df_bottom5_uf = pd.read_sql('SELECT nome_uf_prova, COUNT(*) AS frequencia FROM public.ed_enem_2024_resultados GROUP BY nome_uf_prova ORDER BY frequencia ASC LIMIT 5', engine)
-    desc_uf = pd.read_sql('SELECT nome_uf_prova, COUNT(*) AS frequencia FROM public.ed_enem_2024_resultados GROUP BY nome_uf_prova ORDER BY frequencia DESC', engine)
+    df_top5_uf = pd.read_sql('SELECT nome_uf_prova, COUNT(*) AS frequencia FROM public.ed_enem_2024_resultados_amos_per GROUP BY nome_uf_prova ORDER BY frequencia DESC LIMIT 5', engine)
+    df_bottom5_uf = pd.read_sql('SELECT nome_uf_prova, COUNT(*) AS frequencia FROM public.ed_enem_2024_resultados_amos_per GROUP BY nome_uf_prova ORDER BY frequencia ASC LIMIT 5', engine)
+    desc_uf = pd.read_sql('SELECT nome_uf_prova, COUNT(*) AS frequencia FROM public.ed_enem_2024_resultados_amos_per GROUP BY nome_uf_prova ORDER BY frequencia DESC', engine)
     return df_top5_uf , df_bottom5_uf, desc_uf
 
 @st.cache_data
 def load_matematica():
-    nota_mt = pd.read_sql('SELECT nota_mt_matematica FROM public.ed_enem_2024_resultados WHERE nota_mt_matematica IS NOT NULL', engine)
+    nota_mt = pd.read_sql('SELECT nota_mt_matematica FROM public.ed_enem_2024_resultados_amos_per', engine)
     desc_mt = pd.read_sql('''SELECT
-    COUNT(*) AS count_not_null,
+    COUNT(*) AS count,
     AVG(nota_mt_matematica) AS mean,
-    STDDEV_SAMP(nota_mt_matematica) AS stddev,
+    STDDEV_SAMP(nota_mt_matematica) AS std,
     PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY nota_mt_matematica) AS p25,
     PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY nota_mt_matematica) AS p50,
     PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY nota_mt_matematica) AS p75,
     MAX(nota_mt_matematica) AS max
-    FROM public.ed_enem_2024_resultados
-    WHERE nota_mt_matematica IS NOT NULL''', engine)
+    FROM public.ed_enem_2024_resultados_amos_per''', engine)
     return nota_mt, desc_mt
 
 @st.cache_data
 def load_linguagens():
-    nota_lc = pd.read_sql('SELECT nota_lc_linguagens_e_codigos FROM public.ed_enem_2024_resultados WHERE nota_lc_linguagens_e_codigos IS NOT NULL', engine)
+    nota_lc = pd.read_sql('SELECT nota_lc_linguagens_e_codigos FROM public.ed_enem_2024_resultados_amos_per', engine)
     desc_lc = pd.read_sql('''SELECT
-    COUNT(*) AS count_not_null,
+    COUNT(*) AS count,
     AVG(nota_lc_linguagens_e_codigos) AS mean,
-    STDDEV_SAMP(nota_lc_linguagens_e_codigos) AS stddev,
+    STDDEV_SAMP(nota_lc_linguagens_e_codigos) AS std,
     PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY nota_lc_linguagens_e_codigos) AS p25,
     PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY nota_lc_linguagens_e_codigos) AS p50,
     PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY nota_lc_linguagens_e_codigos) AS p75,
     MAX(nota_lc_linguagens_e_codigos) AS max
-    FROM public.ed_enem_2024_resultados
-    WHERE nota_lc_linguagens_e_codigos IS NOT NULL''', engine)
+    FROM public.ed_enem_2024_resultados_amos_per''', engine)
     return nota_lc, desc_lc
 
 freq_lingua= load_data()
@@ -62,17 +60,13 @@ tab1, tab2, tab3, tab4 = st.tabs(['Frequência por Estado', 'Distribuição por 
 
 with tab1:
 # Top 5 estados com maior frequência
-    fig1 = px.bar(df_top5, x='nome_uf_prova', y='frequencia',
-                title='Estados com maior frequência',
-                color_discrete_sequence=['goldenrod'])
+    fig1 = px.bar(df_top5, x='nome_uf_prova', y='frequencia',title='Estados com maior frequência',color_discrete_sequence=['lightgreen'])
     fig1.update_traces(marker_line_color='black', marker_line_width=1)
     fig1.update_layout(xaxis_tickangle=-40)
     st.plotly_chart(fig1, width='stretch')
 
     # Top 5 estados com menor frequência
-    fig2 = px.bar(df_bottom5, x='nome_uf_prova', y='frequencia',
-                title='Estados com menor frequência',
-                color_discrete_sequence=['steelblue'])
+    fig2 = px.bar(df_bottom5, x='nome_uf_prova', y='frequencia',title='Estados com menor frequência',color_discrete_sequence=['steelblue'])
     fig2.update_traces(marker_line_color='black', marker_line_width=1)
     fig2.update_layout(xaxis_tickangle=-40)
     st.plotly_chart(fig2, width='stretch')
@@ -98,9 +92,17 @@ with tab2:
 
 with tab3:
     #histograma da nota_mt_matematica
-    fig4 = px.histogram(nota_mt, x='nota_mt_matematica', nbins=30, title='Distribuição das Notas de Matemática')
-    st.plotly_chart(fig4, width='stretch')
+    fig4 = px.histogram(nota_mt,x='nota_mt_matematica',nbins=30,title='Distribuição das Notas de Matemática',
+    labels={'nota_mt_matematica': 'Nota de Matemática', 'count': 'Frequência'})
 
+    fig4.update_traces(
+        marker=dict(
+            color='#4F8EF7',
+            line=dict(color='#1A5FC8', width=1.2)
+        )
+    )
+    st.plotly_chart(fig4, use_container_width=True)
+    
     #boxplot da nota_mt_matematica
     fig6 = px.box(nota_mt, y='nota_mt_matematica', title='Boxplot das Notas de Matemática')
     st.plotly_chart(fig6, width='stretch')
@@ -112,7 +114,13 @@ with tab3:
 with tab4:
     #histograma da nota_lc_linguagens_e_codigos
     fig5 = px.histogram(nota_lc, x='nota_lc_linguagens_e_codigos', nbins=30, title='Distribuição das Notas de Linguagens e Códigos')
-    st.plotly_chart(fig5, width='stretch')
+    fig5.update_traces(
+        marker=dict(
+            color='#4F8EF7',
+            line=dict(color='#1A5FC8', width=1.2)
+        )
+    )
+    st.plotly_chart(fig5, use_container_width=True)
 
     #boxplot da nota_lc_linguagens_e_codigos
     fig7 = px.box(nota_lc, y='nota_lc_linguagens_e_codigos', title='Boxplot das Notas de Linguagens e Códigos')
